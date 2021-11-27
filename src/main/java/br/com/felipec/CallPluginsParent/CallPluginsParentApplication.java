1aa1bf96b8502876622f61d756dda982c70e1ba6
Felipe <felipe.carvalho.bs.contato@gmail.com>
2021-11-26 22:30:57 -0300

    feat: initial commit

---
+++ src/main/java/br/com/felipec/CallPluginsParent/CallPluginsParentApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipec.CallPluginsParent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallPluginsParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallPluginsParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/felipec/CallPluginsParent/CallPluginsParentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipec.CallPluginsParent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallPluginsParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
